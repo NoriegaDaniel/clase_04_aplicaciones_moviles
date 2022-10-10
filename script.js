f8d5f5d7eb0639381309237960fd3f3db7b81e3c
NoriegaDaniel <noriegadanielnd@gmail.com>
2022-10-10 13:48:29 -0300

    chore: add initial project files

--- script.js
+++ script.js
@@ -12,18 +12,23 @@ var app = new Vue({
 
 setTimeout(() => {
   app.codigoHtml = '<h3>No te duermas</h3>'
-}, 20000);
+}, 2000);
 
 setTimeout(() => {
   app.codigoHtml = '<h1>Titulo de la página </h1>'
-}, 4000);
+}, 3000);
 
 //app.listaDeElementos.push('Argentina');
 //app.listaDeElementos.push('Perú');
 //app.listaDeElementos.push('Canadá');
 
-app.listaDeElementos.push({texto: "Argentina", aclaracion: "argentino", codigoArea: "+54"})
-app.listaDeElementos.push({texto: "Uruguay", aclaracion: "uruguayo", codigoArea: "+55"})
+app.listaDeElementos.push({ id: 1, texto: "Argentina", aclaracion: "argentino", codigoArea: "+54" })
+app.listaDeElementos.push({ id: 2, texto: "Uruguay", aclaracion: "uruguayo", codigoArea: "+55" })
+
+//Genera un nuevo elemento en listaDeElementos. ... descompone los elementos del objeto y agrega "enable: true"
+app.listaDeElementos = app.listaDeElementos.map(item => {
+  return({ ...item, enable: true });
+})
 
 for (let item of app.listaDeElementos) {
   console.log(item);
